8ac7634e7c1c50cb4ef8f17ea26901fb651cd6de
Alhaytham <asalhaytham@gmail.com>
2018-02-20 10:33:50 +0000

    2

--- src/main/java/uk/ac/le/cs/CO3090/cw1/package-info.java
+++ src/main/java/uk/ac/le/cs/CO3090/cw1/package-info.java
@@ -1,5 +1,10 @@
 /**
+ * Background:
+ * This package is created in response to a requirement given
+ * in file $ThisProjectRootDir/resources/co3090-coursework1.pdf
  * 
+ * Package description: 
+ * TODO
  */
 /**
  * @author alhaytham
