17bdbdfe043b2d20960eed594511e060391795df
Alhaytham <asalhaytham@gmail.com>
2018-02-21 18:27:34 +0000

    tests

--- src/test/java/uk/ac/le/cs/CO3090/cw1/test/InputOutputTest.java
+++ src/test/java/uk/ac/le/cs/CO3090/cw1/test/InputOutputTest.java
@@ -1,10 +1,18 @@
 package uk.ac.le.cs.CO3090.cw1.test;
 
 import org.junit.jupiter.api.*;
-
 import uk.ac.le.cs.CO3090.cw1.InputOutput;
 
-@DisplayName ("Testing Unit InputOutput ... ")
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+/**
+ * This class is  testing InputOutput class
+ */
+
+@DisplayName ("Testing Unit InputOutput: ")
 public class InputOutputTest {
 	static InputOutput inOut;
 	@BeforeAll
@@ -12,6 +20,17 @@ public class InputOutputTest {
 		inOut = new InputOutput();
 	}
 	@Test
+	@DisplayName ("Test Input: ")
 	public void testReadInput() {
+		String sample = "https://blackboard.le.ac.uk,a,b,";
+		byte [] expectedInput = sample.getBytes();
+		
+		
+		InputStream input = new ByteArrayInputStream(expectedInput);
+		inOut.read(input);
+		assertEquals("https://blackboard.le.ac.uk", inOut.getTokens().get(0));
+		assertEquals("a", inOut.getTokens().get(1));
+		assertEquals("b", inOut.getTokens().get(2));
 	}
+
 }
